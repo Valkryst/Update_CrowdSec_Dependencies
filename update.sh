fb26799bf5cc84876f5fb48b192fe73ee4610f25
Valkryst <valkryst@valkryst.com>
2022-02-12 17:54:02 -0400

    Add `http-cve`, `linux-lpe`, `nginx-proxy-manager`, and `pgsql` collections

--- update.sh
+++ update.sh
@@ -6,12 +6,15 @@ cscli hub update
 cscli hub upgrade
 
 
-
+cscli collections upgrade crowdsecurity/base-http-scenarios
+cscli collections upgrade crowdsecurity/http-cve
 cscli collections upgrade crowdsecurity/iptables
 cscli collections upgrade crowdsecurity/linux
-cscli collections upgrade crowdsecurity/sshd
-cscli collections upgrade crowdsecurity/base-http-scenarios
+cscli collections upgrade crowdsecurity/linux-lpe
 cscli collections upgrade crowdsecurity/nginx
+cscli collections upgrade crowdsecurity/nginx-proxy-manager
+cscli collections upgrade crowdsecurity/pgsql
+cscli collections upgrade crowdsecurity/sshd
 
 
 
